bdaaef242b2fe7f328e0bb4229d31b73aeda4a53
kevin Nadeau <kevinnadeau@iMac-2.local>
2019-04-08 16:30:33 +0200

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AuthService } from './auth.service';//Service crée
+import { AngularFireAuthModule } from 'angularfire2/auth';//firebase  authentification
+import { AngularFireModule } from '@angularfire2';//Firebase
+import { Facebook } from '@ionic-native/facebook/ngx';//facebook
+import { environment } from 'src/environments/environment';
+
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.Firebase)],
   providers: [
+    Facebook,
+    AuthService,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  Firebase : {
+    apiKey: "AIzaSyCZ0YjnzF5-_Nt7ZHaC2eZmQteF4MT00Xw",
+    authDomain: "fireproject-c5469.firebaseapp.com",
+    databaseURL: "https://fireproject-c5469.firebaseio.com",
+    projectId: "fireproject-c5469",
+    storageBucket: "fireproject-c5469.appspot.com",
+    messagingSenderId: "302859139305"
+  }
+
 };
